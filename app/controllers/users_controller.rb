64e249dbd79a0ba311e21c694afa5d90bef6889d
neo <hayounn.etd@gmail.com>
2022-10-27 17:34:33 +0100

    Fix linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,18 @@
 class UsersController < ApplicationController
   before_action :authorize_request, except: :create
   before_action :find_user, except: %i[create index]
-  
+
   # GET /users
   def index
     @users = User.all
     render json: @users, status: :ok
   end
-  
+
   # GET /users/{username}
   def show
     render json: @user, status: :ok
   end
-  
+
   # POST /users
   def create
     @user = User.new(user_params)
@@ -23,26 +23,26 @@ class UsersController < ApplicationController
              status: :unprocessable_entity
     end
   end
-  
+
   # PUT /users/{username}
   def update
-    unless @user.update(user_params)
+    unless @user.update(user_params) # rubocop:todo Style/GuardClause
       render json: { errors: @user.errors.full_messages },
              status: :unprocessable_entity
     end
   end
-  
+
   # DELETE /users/{username}
   def destroy
     @user.destroy
   end
-  
+
   def find_user
     @user = User.find_by_username!(params[:_username])
-    rescue ActiveRecord::RecordNotFound
-      render json: { errors: 'User not found' }, status: :not_found
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: 'User not found' }, status: :not_found
   end
-  
+
   def user_params
     params.permit(
       :avatar, :name, :username, :email, :password, :password_confirmation
