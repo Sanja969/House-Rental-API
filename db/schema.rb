c3f28603135055d474308f7e86a02b48e216b0a5
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-10-27 19:20:30 +0300

    remove name column from user table

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_133938) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "username"
     t.string "email"
     t.string "password_digest"
