0bae68b778fea7089af9c032474052efa5cb4e64
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-10-28 15:57:29 +0300

    Add user relation with reservation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :reservations, foreign_key: 'user_id'
   # mount_uploader :avatar, AvatarUploader
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
