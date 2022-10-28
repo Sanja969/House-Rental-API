e53b59149a60603a658a35a83e66f1a8b2736379
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-10-28 16:23:07 +0300

    Add migration to add house_id to reservation

--- db/migrate/20221028131827_add_house_to_reservations.rb
+++ db/migrate/20221028131827_add_house_to_reservations.rb
@@ -1,5 +1,5 @@
 class AddHouseToReservations < ActiveRecord::Migration[7.0]
   def change
-    add_reference :reservations, :house, null: false, foreign_key: true
+    add_reference :reservations, :house, null: false, foreign_key: {to_table: :houses}
   end
 end
