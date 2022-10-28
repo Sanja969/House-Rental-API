ee9fce35c2f3f7831be8fc4510d4662e3d5d3cf0
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-10-28 16:58:03 +0300

    Fix linters

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,27 +1,29 @@
 class ReservationsController < ApplicationController
+  def index
+    @reservations = Reservation.all
+  end
 
-    def index
-      @reservations = Reservation.all
-    end
+  def create
+      # rubocop:todo Layout/IndentationWidth
+      @reservation = Reservation.create(status: 'pending', user_id: params[:user_id], house_id: params[:house_id],
+                                        date: params[:date])
+      # rubocop:enable Layout/IndentationWidth
+      render json: @reservation, status: :ok
 
-    def create
-        @reservation = Reservation.create( status: 'pending' , user_id: params[:user_id], house_id: params[:house_id], date: params[:date])
-        render json: @reservation, status: :ok
-      
-        if @reservation.save
-            render json: @reservation, status: :ok
-        else
-          render json: { error: 'Reservation not denied' }, status: :unprocessable_entity
-        end
-    end
+      if @reservation.save
+          render json: @reservation, status: :ok # rubocop:todo Layout/IndentationWidth
+      else
+        render json: { error: 'Reservation not denied' }, status: :unprocessable_entity
+      end
+  end
 
-    def cancel_reservation
-        @cancel = Reservation.find(params[:id])
-        @cancel.destroy
-        if @cancel.destroyed?
-          render json: { message: 'Booking cancelled successfully.' }, status: :ok
-        else
-          render json: { message: 'Booking not cancelled.' }, status: :unprocessable_entity
-        end
-    end
+  def cancel_reservation
+      @cancel = Reservation.find(params[:id]) # rubocop:todo Layout/IndentationWidth
+      @cancel.destroy
+      if @cancel.destroyed?
+        render json: { message: 'Booking cancelled successfully.' }, status: :ok
+      else
+        render json: { message: 'Booking not cancelled.' }, status: :unprocessable_entity
+      end
+  end
 end
