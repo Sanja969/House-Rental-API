96c5b1071a237643b901f6eace2dcd654e4f529e
Sanja <sanja1mandic@gmail.com>
2022-11-28 14:52:22 +0100

    Fix deploy bug for render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,8 @@ Rails.application.configure do
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
 
+  config.assets.compile = true
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
