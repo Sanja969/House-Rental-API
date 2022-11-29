6aaa37c8431ac2dd5b353aa1f725f4f5ec81016f
Sanja <sanja1mandic@gmail.com>
2022-11-29 13:26:37 +0100

    d

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,11 +5,4 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
 bundle exec rake db:migrate
